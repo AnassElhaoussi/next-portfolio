20356b19dccf038c489cb8ae2096e7a9540d7ed3
anasselhaoussi <anous.elhaoussi@gmail.com>
2023-02-26 18:51:38 +0100

    style: setting up the files, configuring the app directory and using google fonts api

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export {default as Home} from './Home'
+export {default as About} from './About'
+export {default as Navbar} from './Navbar'
+export {default as Footer} from './Footer'
+export {default as Contact} from './Contact'
---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/globals.css'
+import '../styles/index.css'
+
+function RootLayout({children}: {
+    children: React.ReactNode
+}) {
+  return (
+    <html>
+        <head>
+            <link href="https://fonts.googleapis.com/css2?family=Abel&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        </head>
+        <body>{children}</body>
+    </html>
+  )
+}
+export default RootLayout
